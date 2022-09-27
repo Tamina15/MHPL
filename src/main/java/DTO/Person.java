a1b05e6ad977ffc70e31ba50fd159aabb02607c1
Tamina15 <minhnhat66442658@gmail.com>
2022-09-27 11:18:00 +0700

    student

---
+++ src/main/java/DTO/Person.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DTO;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Tran Minh Nhat
+ */
+public class Person {
+
+    private int PersonID;
+    private String LastName, FirstName;
+    private Date EnrollmentDate, HireDate;
+
+    public Person() {
+
+    }
+
+    public Person(int id, String lastname, String firstname, Date hiredate, Date enrollmentdate) {
+        PersonID = id;
+        LastName = lastname;
+        FirstName = firstname;
+        EnrollmentDate = enrollmentdate;
+        HireDate = hiredate;
+    }
+
+    public int getPersonID() {
+        return PersonID;
+    }
+
+    public void setPersonID(int PersonID) {
+        this.PersonID = PersonID;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public Date getEnrollmentDate() {
+        return EnrollmentDate;
+    }
+
+    public void setEnrollmentDate(Date EnrollmentDate) {
+        this.EnrollmentDate = EnrollmentDate;
+    }
+
+    public Date getHireDate() {
+        return HireDate;
+    }
+
+    public void setHireDate(Date HireDate) {
+        this.HireDate = HireDate;
+    }
+
+    public String getFullName() {
+        return FirstName + " " + LastName;
+    }
+}
--- src/main/java/DTO/Student.java
+++ src/main/java/DTO/Student.java
@@ -11,56 +11,23 @@ import java.sql.Date;
  *
  * @author Tran Minh Nhat
  */
-public class Student {
-
-    private int StudentID;
-    private String LastName, FirstName;
-    private Date EnrollmentDate;
+public class Student extends Person {
 
     public Student() {
 
     }
 
     public Student(int id, String lastname, String firstname, Date date) {
-        StudentID = id;
-        LastName = lastname;
-        FirstName = firstname;
-        EnrollmentDate = date;
+        super(id, lastname, firstname, null, date);
     }
 
-    public int getStudentID() {
-        return StudentID;
+    @Override
+    public Date getHireDate() {
+        return null;
     }
 
-    public void setStudentID(int StudentID) {
-        this.StudentID = StudentID;
-    }
-
-    public String getLastName() {
-        return LastName;
-    }
-
-    public void setLastName(String LastName) {
-        this.LastName = LastName;
-    }
-
-    public String getFirstName() {
-        return FirstName;
-    }
-
-    public void setFirstName(String FirstName) {
-        this.FirstName = FirstName;
-    }
-
-    public Date getEnrollmentDate() {
-        return EnrollmentDate;
-    }
-
-    public void setEnrollmentDate(Date EnrollmentDate) {
-        this.EnrollmentDate = EnrollmentDate;
-    }
-
-    public String getFullName() {
-        return FirstName + " " + LastName;
+    @Override
+    public void setHireDate(Date HireDate) {
+        HireDate = null;
     }
 }
